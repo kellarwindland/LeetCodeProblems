862bd4868a19f5d6a1b5f3b20dbaa55517567b10
Kellar  Windland <windland.6@buckeyemail.osu.edu>
2020-05-22 14:05:14 -0400

    Update problem1450.java

--- problem1450.java
+++ problem1450.java
@@ -1,3 +1,8 @@
+// Given two integer arrays startTime and endTime and given an integer queryTime.
+// The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+// Return the number of students doing their homework at time queryTime. More formally, return the number of
+// students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+
 class Solution {
     public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
         
