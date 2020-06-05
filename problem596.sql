1d1d8013cbd463d3eff4178c210e82ab0d0004a9
Kellar  Windland <windland.6@buckeyemail.osu.edu>
2020-06-05 16:14:33 -0400

    Create problem596.sql

---
+++ problem596.sql
@@ -0,0 +1,7 @@
+// There is a table courses with columns: student and class
+// Please list out all classes which have more than or equal to 5 students.
+
+SELECT class
+FROM courses
+GROUP BY class
+HAVING COUNT(DISTINCT student) >= 5;
