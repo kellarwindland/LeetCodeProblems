b02e43ddd21a4ff1940162fd7b037b16a9a4e6f7
Kellar  Windland <windland.6@buckeyemail.osu.edu>
2020-09-26 21:39:17 -0400

    Create RemoveOutermostParentheses.java

---
+++ RemoveOutermostParentheses.java
@@ -0,0 +1,38 @@
+// A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation. 
+// For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+// A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+// Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+// Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+
+class Solution {
+    public String removeOuterParentheses(String S) {
+        
+        Set<Integer> indicies = new HashSet<>();
+        int count = 0;
+        for(int i = 0; i < S.length(); i++){
+            if(count == 0){
+                indicies.add(i);
+            }
+            
+            if(S.charAt(i) == '('){
+                count++;
+            }else{
+                count--;
+            }
+            
+            if(count == 0){
+                indicies.add(i);
+            }
+        }
+        
+        String result = "";
+        for(int i = 0; i < S.length(); i++){
+            if(!indicies.contains(i)){
+                result += S.charAt(i);
+            }
+        }
+        
+        return result;
+    }
+}
