e27c2873b0277aed65d5d3764101d3bca6b3cc10
Kellar  Windland <windland.6@buckeyemail.osu.edu>
2020-09-29 19:07:36 -0400

    Create ThreeConsecutiveOdds.java

---
+++ ThreeConsecutiveOdds.java
@@ -0,0 +1,16 @@
+// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        
+        for(int i = 0; i < arr.length - 2; i++){
+            
+            if((arr[i] % 2 == 1) && (arr[i + 1] % 2 == 1) && (arr[i + 2] % 2 == 1)){
+                return true;    
+            }
+            
+        }
+        
+        return false;
+    }
+}
