ec83ea6c4e9ae06423151cb33a04b6b92f6d7917
Kellar  Windland <windland.6@buckeyemail.osu.edu>
2020-09-30 11:29:50 -0400

    Update BinaryTreePreorderTraversal.java

--- BinaryTreePreorderTraversal.java
+++ BinaryTreePreorderTraversal.java
@@ -19,3 +19,30 @@ class Solution {
         dfs(root.right, result);
     }
 }
+
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+        
+//         List<Integer> result = new LinkedList<>();
+//         if(root == null){
+//             return result;
+//         }
+//         Stack<TreeNode> stack = new Stack<>();
+//         stack.push(root);
+        
+//         while(!stack.isEmpty()){
+//             TreeNode curr = stack.pop();
+//             result.add(curr.val);
+            
+//             if(curr.right != null){
+//                 stack.push(curr.right);
+//             }
+//             if(curr.left != null){
+//                 stack.push(curr.left);
+//             }
+//         }
+        
+//         return result;
+//     }
+
+// }
