d1aee14ec9a587d65dcd798e505f5782d49a5306
Kellar  Windland <windland.6@buckeyemail.osu.edu>
2021-02-24 21:54:59 -0500

    Update AverageSalaryExcludingMaxAndMin.java

--- AverageSalaryExcludingMaxAndMin.java
+++ AverageSalaryExcludingMaxAndMin.java
@@ -1,3 +1,6 @@
+// Given an array of unique integers salary where salary[i] is the salary of the employee i.
+// Return the average salary of employees excluding the minimum and maximum salary.
+
 class Solution {
     public double average(int[] salary) {
         Arrays.sort(salary);
