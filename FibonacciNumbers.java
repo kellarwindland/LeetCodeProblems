b85a253acc49e2f19347e041935b37dcd63db151
Kellar  Windland <windland.6@buckeyemail.osu.edu>
2021-03-04 12:16:20 -0500

    Create FibonacciNumbers.java

---
+++ FibonacciNumbers.java
@@ -0,0 +1,16 @@
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+// such that each number is the sum of the two preceding ones, starting from 0 and 1. 
+
+class Solution {
+    public int fib(int n) {
+        if(n == 0){
+            return 0;
+        }
+        
+        if(n == 1){
+            return 1;
+        }
+        
+        return fib(n - 1) + fib(n - 2);
+    }
+}
