aa8943aac0f1712b7673fc5ff51564b1eb92e651
Kellar  Windland <windland.6@buckeyemail.osu.edu>
2021-03-03 22:14:15 -0500

    Create ContainsDuplicateII.java

---
+++ ContainsDuplicateII.java
@@ -0,0 +1,17 @@
+// Given an array of integers and an integer k, find out whether there are two distinct
+// indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] == nums[j] && Math.abs(i - j) <= k){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
